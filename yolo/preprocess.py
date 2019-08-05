7bd178a08dedc78b7843887565bc25377cfc5321
Ed Bartrum <edward.bartrum@gmail.com>
2019-08-05 15:41:13 +0100

    Create method for image preprocessing directly on image array

--- yolo/preprocess.py
+++ yolo/preprocess.py
@@ -30,6 +30,18 @@ def letterbox_image(img, inp_dim):
 
     return canvas
 
+def prep_image_array(orig_im, inp_dim):
+    """
+    Prepare image for inputting to the neural network.
+
+    Returns a Variable
+    """
+
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
 
 def prep_image(img, inp_dim):
     """
@@ -39,12 +51,7 @@ def prep_image(img, inp_dim):
     """
 
     orig_im = cv2.imread(img)
-    dim = orig_im.shape[1], orig_im.shape[0]
-    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
-    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
-    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
-    return img_, orig_im, dim
-
+    return prep_image_array(orig_im, inp_dim)
 
 def prep_frame(img, inp_dim):
     """
